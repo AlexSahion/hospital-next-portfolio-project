76a4117f97235f18883ad7daa710454de9bec8dd
AlexSahion <saha.las399@gmail.com>
2025-05-06 16:01:30 +0200

    add: all components of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import css from './page.module.scss'
+import { BaseContainer } from "@/shared/ui";
+import { PostFeed, RightColumn } from "@/widgets";
+import { Sidebar } from "@/widgets";
+
 export default function Home() {
 	return (
-		<></>
+		<BaseContainer className={css.container}>
+			<Sidebar className={css.sidebar} />
+			<PostFeed className={css.posts} />
+			<RightColumn className={css.right} />
+		</BaseContainer>
 	);
 }
