97c43efa84bd1656595f37ab78ffe4bce38af776
AlexSahion <saha.las399@gmail.com>
2025-05-10 19:22:20 +0200

    new: online school widget

--- src/widgets/online-school-widget/online-school-widget.tsx
+++ src/widgets/online-school-widget/online-school-widget.tsx
@@ -1,10 +1,27 @@
 import css from './online-school-widget.module.scss'
 import cn from 'classnames'
+import Image from 'next/image'
+import { OnlineSchoolButton } from '@/features/online-school-button'
+import { Logo } from '@/shared/ui'
+
 
 export const OnlineSchoolWidget = ({ className }: { className?: string }) => {
 	return (
 		<section className={cn(css.school, className)}>
-			<h2>OnlineSchoolWidget</h2>
+			<div className={css.content}>
+				<Logo height={50} width={163} isWhite={true} />
+				<div className={css.title}>Онлайн школа</div>
+				<div className={css.img}>
+					<Image
+						alt='online-school-img'
+						src={'/assets/online-school/doctor.png'}
+						width={228}
+						height={222}
+					/>
+				</div>
+				<div className={css.subtitle}>Первый урок бесплатно!</div>
+				<OnlineSchoolButton />
+			</div>
 		</section>
 	)
 }
