574019eeb668ec9d4468737e8f36897acbb7cc0b
AlexSahion <saha.las399@gmail.com>
2025-05-10 17:53:33 +0200

    add: structure for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import css from './page.module.scss'
 import { BaseContainer } from "@/shared/ui";
-import { PostFeed, RightColumn } from "@/widgets";
+import { Events, HeaderBanner, OnlineSchoolWidget, PostFeed, Stats } from "@/widgets";
 import { Sidebar } from "@/widgets";
 
 export default function Home() {
 	return (
 		<BaseContainer className={css.container}>
 			<Sidebar className={css.sidebar} />
-			<PostFeed className={css.posts} />
-			<RightColumn className={css.right} />
+			<div className={css.content}>
+				<div className={css.posts}>
+					<HeaderBanner />
+					<PostFeed className={css.feed} />
+				</div>
+				<div className={css.right}>
+					<Stats />
+					<Events />
+					<OnlineSchoolWidget />
+				</div>
+			</div>
 		</BaseContainer>
 	);
 }
