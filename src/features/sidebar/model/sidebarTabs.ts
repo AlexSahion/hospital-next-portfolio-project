c116c992e38d438d1d4e7a493260afc66bdf2c61
AlexSahion <saha.las399@gmail.com>
2025-05-10 19:20:30 +0200

    fix: add link and type of routerPath

--- src/features/sidebar/model/sidebarTabs.ts
+++ src/features/sidebar/model/sidebarTabs.ts
@@ -1,21 +1,26 @@
 import { LayoutGrid, MessageCircleMoreIcon } from "lucide-react"
+import { routerMap } from "@/shared/const"
+import { RouterPath } from "@/shared/const"
 
 interface SideBarTabs {
 	id: string,
 	label: string,
-	icon: React.ElementType
+	icon: React.ElementType,
+	link: RouterPath
 }
 
 export const sideBarTabs: SideBarTabs[] = [
 	{
 		id: 'feed',
 		label: "Лента",
-		icon: LayoutGrid
+		icon: LayoutGrid,
+		link: routerMap.home
 	},
 
 	{
 		id: 'message',
 		label: "Сообщения",
-		icon: MessageCircleMoreIcon
+		icon: MessageCircleMoreIcon,
+		link: routerMap.message
 	},
 ]
