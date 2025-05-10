6003c2ddb7dbccf3e60b46d594edd39fefd9d3fe
AlexSahion <saha.las399@gmail.com>
2025-05-10 17:54:32 +0200

    add: root file for events

---
+++ src/app/api/events/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+	return NextResponse.json([
+		{
+			avatar: '/assets/events/avatar-1.png',
+			name: 'Девятых Т.',
+			position: 'Реаниматолог',
+			time: '1 д.',
+			text: 'Уже завтра - поэтому срочно в номер - стартует продолжение серии вебинаров об ультразвуковой сосудистой визуализации с прекрасной Людмилой Эдуардовной Шульгиной.'
+		},
+
+		{
+			avatar: '/assets/events/avatar-2.png',
+			name: 'Doctor News. D.',
+			position: 'Frontend',
+			time: '1 д.',
+			text: 'Уже завтра - поэтому срочно в номер - стартует продолжение серии вебинаров об ультразвуковой сосудистой визуализации с прекрасной Людмилой Эдуардовной Шульгиной.'
+		},
+	])
+}
