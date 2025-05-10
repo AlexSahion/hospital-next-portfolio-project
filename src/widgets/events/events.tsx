0990fcbdccea8587584b70f4555557979a418a64
AlexSahion <saha.las399@gmail.com>
2025-05-10 17:58:56 +0200

    new: events widget component

---
+++ src/widgets/events/events.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useEffect } from 'react'
+import css from './events.module.scss'
+import cn from 'classnames'
+import { useEventItem } from '@/entities/event-item'
+import { EventItem } from '@/entities/event-item'
+import { TextWeigth } from '@/shared/ui'
+
+import { usePathname, useRouter } from 'next/navigation'
+
+export const Events = ({ className }: { className?: string }) => {
+	const items = useEventItem()
+
+	return (
+		<section className={cn(css.stats, className)}>
+			<div className={css.top}>
+				<h3><TextWeigth text='Back' /></h3>
+			</div>
+			<div className={css.content}>
+				{items?.map((item) => (
+					<EventItem key={item.avatar} {...item} />
+				))}
+			</div>
+		</section>
+	)
+}
