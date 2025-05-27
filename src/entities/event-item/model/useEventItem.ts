f1ae582d254ba119958db2053b0787a2b3908a8f
AlexSahion <saha.las399@gmail.com>
2025-05-27 16:34:11 +0200

    fix: delegetion of response and get data

--- src/entities/event-item/model/useEventItem.ts
+++ src/entities/event-item/model/useEventItem.ts
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react"
+import { getEvents } from "../api";
 import { EventItemProps } from "./types"
 
 export const useEventItem = () => {
 	const [events, setEvents] = useState<EventItemProps[] | null>(null)
 
 	useEffect(() => {
-		const getEvents = async () => {
-			const res = await fetch('/api/events')
-			const data: EventItemProps[] = await res.json()
-			setEvents(data)
-		}
-
 		getEvents()
+			.then(setEvents)
+			.catch(err => {
+				console.error(err)
+				setEvents([])
+			})
 	}, [])
 
 	return events
