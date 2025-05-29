0f70450720e96e8c7e7425e3080bdb7b0102111b
AlexSahion <saha.las399@gmail.com>
2025-05-29 15:51:04 +0200

    add: content for postFeed component

--- src/widgets/post-feed/post-feed.tsx
+++ src/widgets/post-feed/post-feed.tsx
@@ -1,11 +1,20 @@
+'use client'
+
 import css from './post-feed.module.scss'
 import cn from 'classnames'
+import { UserPostAction } from '@/features/user-post-action';
+import { ThankButton } from '@/features/thank-button';
+import { usePostCard } from '@/entities/post-card/model/use-post-card';
 import { PostCard } from '@/entities/post-card';
 
 export const PostFeed = ({ className }: { className?: string }) => {
+	const posts = usePostCard()?.slice(0, 6)
+
 	return (
 		<div className={cn(css.container, className)}>
-			<PostCard />
+			{posts?.map(params => (
+				<PostCard key={params.id} params={params} thank={ThankButton} actions={<UserPostAction />} />
+			))}
 		</div>
 	)
 }
